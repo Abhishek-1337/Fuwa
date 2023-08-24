15618246b3c78321fe94db83386e31bde26a8a1f
Abhishek-1337 <abhishekrinku4@gmail.com>
2023-08-24 17:54:02 +0530

    Frontend intialized

---
+++ fuwa-frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Dashboard from "./components/dashboard/Dashboard";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="/" element={<Login />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
