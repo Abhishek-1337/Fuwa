3586a99200cad9db9e4dd8657c7e051817fbbcca
Abhishek-1337 <abhishekrinku4@gmail.com>
2023-11-21 18:29:42 +0530

    Created slice for friend invitation to keep track of the invitee

--- fuwa-frontend/src/store/index.js
+++ fuwa-frontend/src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./slices/authSlice";
 import alertSlice from "./slices/alertSlice";
+import friendSlice from "./slices/friendsSlice";
 
 const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
     alert: alertSlice.reducer,
+    friend: friendSlice.reducer,
   },
 });
 
