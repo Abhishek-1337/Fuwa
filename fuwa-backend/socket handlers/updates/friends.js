fcd4cd2e03eff797b379cd71c71926cb74f64da3
Abhishek-1337 <abhishekrinku4@gmail.com>
2024-01-07 17:56:19 +0530

    after accepting invite update friends for user

--- fuwa-backend/socket handlers/updates/friends.js
+++ fuwa-backend/socket handlers/updates/friends.js
@@ -1,4 +1,5 @@
 const FriendInvitation = require("../../models/friendInvitationModel");
+const User = require("../../models/userModel");
 const {
   getActiveConnection,
   getSocketServerInstance,
@@ -13,12 +14,47 @@ exports.updatePendingFriendInvitation = async (userId) => {
     //get all active connection of user
     const recieveList = getActiveConnection(userId);
     const io = getSocketServerInstance();
-
+    console.log("recieveList");
+    console.log(recieveList);
     recieveList.forEach((recieverSocketId) => {
       io.to(recieverSocketId).emit("friend-invitation", {
         pendingFriendInvitations: pendingInvitation ? pendingInvitation : [],
       });
     });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.updateFriends = async (userId) => {
+  try {
+    const recieverList = getActiveConnection(userId);
+    if (recieverList.length > 0) {
+      const user = await User.findById(userId, { _id: 1, friends: 1 }).populate(
+        {
+          path: "friends",
+          select: "_id name email",
+        }
+      );
+
+      if (user) {
+        const friendsList = user.friends.map((friend) => {
+          return {
+            id: friend._id,
+            name: friend.name,
+            email: friend.email,
+          };
+        });
+
+        const io = getSocketServerInstance();
+
+        recieverList.forEach((recieveSocketId) => {
+          io.to(recieveSocketId).emit("friends-list", {
+            friends: friendsList ? friendsList : [],
+          });
+        });
+      }
+    }
   } catch (err) {
     console.log(err);
   }
