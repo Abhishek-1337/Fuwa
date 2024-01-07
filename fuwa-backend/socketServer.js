522d76e4a812aad0239bf7dd97fa5e6c1823ebc7
Abhishek-1337 <abhishekrinku4@gmail.com>
2024-01-07 17:53:13 +0530

    Broadcast online users list to client

--- fuwa-backend/socketServer.js
+++ fuwa-backend/socketServer.js
@@ -1,7 +1,7 @@
 const newConnectionHandler = require("./socket handlers/newConnectionHandler");
 const authSocket = require("./controllers/authSocket");
 const disconnectHandler = require("./socket handlers/disconnectHandler");
-const { setSocketServerInstance } = require("./serverStore");
+const { setSocketServerInstance, getOnlineUsers } = require("./serverStore");
 
 const registerSocketServer = (server) => {
   const io = require("socket.io")(server, {
@@ -19,15 +19,25 @@ const registerSocketServer = (server) => {
 
   //Same connect event but when client is connected to server.
   //We have only one instance of io and many instances of socket according to the number of devices connected
+
+  const emitOnlineUsers = () => {
+    const onlineUsers = getOnlineUsers();
+    io.emit("online-users", { onlineUsers });
+  };
+
   io.on("connection", (socket) => {
     console.log("user connected");
     console.log(socket.id);
     newConnectionHandler(socket, io);
-
+    emitOnlineUsers();
     socket.on("disconnect", () => {
       disconnectHandler(socket);
     });
   });
+
+  setInterval(() => {
+    emitOnlineUsers();
+  }, [8 * 1000]);
 };
 
 module.exports = {
