e3063413f7f1e97540b6381d7bc39bf7727611e0
Abhishek-1337 <abhishekrinku4@gmail.com>
2024-01-07 17:52:40 +0530

    get online users from connected socket user

--- fuwa-backend/serverStore.js
+++ fuwa-backend/serverStore.js
@@ -12,15 +12,15 @@ exports.getSocketServerInstance = () => {
 
 exports.setConnectedUser = ({ socketId, userId }) => {
   connectedUser.set(socketId, { userId });
+  console.log("New connected user");
   console.log(connectedUser);
 };
 
 exports.removeConnectedUser = (socketId) => {
   if (connectedUser.has(socketId)) {
     connectedUser.delete(socketId);
+    console.log("New connected users after deletion", connectedUser);
   }
-  console.log("After deletion");
-  console.log(connectedUser);
 };
 
 let activeConnection = [];
@@ -32,4 +32,11 @@ exports.getActiveConnection = (userId) => {
     }
   });
   return activeConnection;
+};
+
+exports.getOnlineUsers = (userId) => {
+  let onlineUsers = [];
+  connectedUser.forEach((value, key) => {
+    onlineUsers.push({ socketId: key, userId: value.userId });
+  });
 };
