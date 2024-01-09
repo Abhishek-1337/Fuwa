da0747e581cdd93b4536acdfa5a0cd37b88ceb05
Abhishek-1337 <abhishekrinku4@gmail.com>
2024-01-09 14:07:49 +0530

    Input field for messages added

--- fuwa-frontend/src/components/dashboard/Messenger/MessageInput.jsx
+++ fuwa-frontend/src/components/dashboard/Messenger/MessageInput.jsx
@@ -1,5 +1,51 @@
-const MessageInput = () => {
-  return <div></div>;
+import { styled } from "@mui/material";
+import { useState } from "react";
+
+const MainContainer = styled("div")({
+  height: "60px",
+  width: "100%",
+  display: "flex",
+  alignItems: "center",
+});
+
+const Input = styled("input")({
+  backgroundColor: "#2f3136",
+  height: "98%",
+  width: "100%",
+  borderRadius: "8px",
+  fontSize: "14px",
+  border: "none",
+  padding: "0 10px",
+  textAlign: "center",
+});
+
+const MessageInput = ({ chosenChatDetails }) => {
+  const [message, setMessage] = useState("");
+
+  const handleKeyOnChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleOnPress = (e) => {
+    if (e.key === "Enter") {
+      handleSendMessage();
+    }
+  };
+
+  const handleSendMessage = () => {
+    console.log("Sending message to server");
+    setMessage("");
+  };
+  return (
+    <MainContainer>
+      <Input
+        placeholder={`Write message to ${chosenChatDetails?.name}`}
+        onKeyDown={handleOnPress}
+        onChange={handleKeyOnChange}
+        value={message}
+      />
+    </MainContainer>
+  );
 };
 
 export default MessageInput;
