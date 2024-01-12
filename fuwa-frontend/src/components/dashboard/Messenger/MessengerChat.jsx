7a8367f60bdac71d4c2d5ea0649fd24368245a2a
Abhishek-1337 <abhishekrinku4@gmail.com>
2024-01-12 19:23:59 +0530

    Fetch chat history when opening a inbox for a specific user

--- fuwa-frontend/src/components/dashboard/Messenger/MessengerChat.jsx
+++ fuwa-frontend/src/components/dashboard/Messenger/MessengerChat.jsx
@@ -2,6 +2,7 @@ import { styled } from "@mui/material";
 import Messages from "./Messages/Messages";
 import MessageInput from "./MessageInput";
 import { useEffect } from "react";
+import { getDirectChatHistory } from "../../../realtimeCommunication/socketConnection";
 
 const Wrapper = styled("div")({
   flexGrow: 1,
@@ -13,6 +14,9 @@ const Wrapper = styled("div")({
 const MessengerChat = ({ chosenChatDetails }) => {
   useEffect(() => {
     //fetch chat history of specific user with user id
+    getDirectChatHistory({
+      recieverId: chosenChatDetails.id,
+    });
   }, [chosenChatDetails]);
   return (
     <Wrapper>
