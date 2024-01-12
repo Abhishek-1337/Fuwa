093a391a779334b3069556062c5e9cdfc7d733ce
Abhishek-1337 <abhishekrinku4@gmail.com>
2024-01-12 19:24:40 +0530

    Rendered messages from backend with date in readable form

--- fuwa-frontend/src/components/dashboard/Messenger/Messages/Messages.jsx
+++ fuwa-frontend/src/components/dashboard/Messenger/Messages/Messages.jsx
@@ -1,8 +1,8 @@
 import { styled } from "@mui/material";
 import MessageHeader from "./MessageHeader";
 import { useSelector } from "react-redux";
-import DUMMY_MESSAGES from "./DUMMY_MESSAGES";
 import Message from "./Message";
+import DateSeparator from "./DateSeparator";
 
 const MainContainer = styled("div")({
   height: "calc(100%-100px)",
@@ -10,21 +10,61 @@ const MainContainer = styled("div")({
   flexDirection: "column",
 });
 
+const convertDateToHumanReadable = (date, format) => {
+  const map = {
+    mm: date.getMonth() + 1,
+    dd: date.getDate(),
+    yy: date.getFullYear().toString().slice(-2),
+    yyyy: date.getFullYear(),
+  };
+  return format.replace(/mm|dd|yy|yyyy/gi, (matched) => {
+    return map[matched];
+  });
+};
+
 const Messages = () => {
   const chat = useSelector((state) => state.chat);
+
   return (
     <MainContainer>
       <MessageHeader name={chat.chosenChatDetails?.name} />
-      {DUMMY_MESSAGES.map((message) => {
+      {chat.messages?.map((message, index) => {
+        const sameAuthor =
+          index > 0 &&
+          message[index]?.author._id === message[index - 1]?.author._id;
+
+        const sameDay =
+          index > 0 &&
+          convertDateToHumanReadable(
+            new Date(message[index]?.date),
+            "dd/mm/yy"
+          ) ===
+            convertDateToHumanReadable(
+              new Date(message[index - 1]?.date),
+              "dd/mm/yy"
+            );
+
         return (
-          <Message
-            key={message._id}
-            content={message.content}
-            name={message.author.username}
-            sameAuthor={message.sameAuthor}
-            date={message.date}
-            sameDay={message.sameDay}
-          />
+          <div key={message._id}>
+            {(!sameDay || index === 0) && (
+              <DateSeparator
+                date={convertDateToHumanReadable(
+                  new Date(message?.date),
+                  "dd/mm/yy"
+                )}
+              />
+            )}
+            <Message
+              content={message.content}
+              name={message.author.name}
+              sameAuthor={sameAuthor}
+              date={convertDateToHumanReadable(
+                new Date(message?.date),
+                "dd/mm/yy"
+              )}
+              sameDay={sameDay}
+            />
+          </div>
         );
       })}
     </MainContainer>
