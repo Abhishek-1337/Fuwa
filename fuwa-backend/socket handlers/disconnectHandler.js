f1043fb0aee8a952159a764859842060d4498a7b
Abhishek-1337 <abhishekrinku4@gmail.com>
2024-01-29 13:53:35 +0530

    backend: leave room when disconnect

--- fuwa-backend/socket handlers/disconnectHandler.js
+++ fuwa-backend/socket handlers/disconnectHandler.js
@@ -1,7 +1,19 @@
 const serverStore = require("../serverStore");
+const room = require("./updates/room");
 
 const disconnectHandler = (socket) => {
   serverStore.removeConnectedUser(socket.id);
+
+  const activeRooms = serverStore.getActiveRooms();
+  activeRooms.forEach((activeRoom) => {
+    const userInRoom = activeRoom.participants.some(
+      (participant) => participant.socketId === socket.id
+    );
+    if (userInRoom) {
+      serverStore.leaveActiveRoom({ roomId: activeRoom.roomId }, socket);
+      room.updateRooms();
+    }
+  });
 };
 
 module.exports = disconnectHandler;
