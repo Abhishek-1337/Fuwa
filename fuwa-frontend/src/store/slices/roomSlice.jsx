044b20ac420649e36071874716d5a402d1272102
Abhishek-1337 <abhishekrinku4@gmail.com>
2024-01-29 13:51:24 +0530

    Created store for room

---
+++ fuwa-frontend/src/store/slices/roomSlice.jsx
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isUserInRoom: false,
+  isUserRoomCreator: false,
+  roomDetails: null,
+  activeRooms: [],
+  localStream: null,
+  isLocalStream: true,
+  remoteStreams: [],
+  audioOnly: false,
+  screenSharingStream: null,
+  isScreenSharingActive: false,
+};
+
+const roomSlice = createSlice({
+  name: "room",
+  initialState,
+  reducers: {
+    setOpenRoom(state, action) {
+      state.isUserInRoom = action.payload.isUserInRoom;
+      state.isUserRoomCreator = action.payload.isUserRoomCreator;
+    },
+    setRoomDetails(state, action) {
+      state.roomDetails = action.payload;
+    },
+    setActiveRooms(state, action) {
+      state.activeRooms = action.payload;
+    },
+    setLocalStream(state, action) {
+      state.localStream = action.payload;
+    },
+  },
+});
+
+export const roomActions = roomSlice.actions;
+export default roomSlice;
