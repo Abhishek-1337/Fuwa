a3171be3c3a0e1c5e7e11ad590867000f67c32d6
Abhishek-1337 <abhishekrinku4@gmail.com>
2024-01-29 13:56:56 +0530

    webRTC handler for local stream preview

---
+++ fuwa-frontend/src/realtimeCommunication/webRTCHandler.js
@@ -0,0 +1,27 @@
+import store from "../store";
+import { roomActions } from "../store/slices/roomSlice";
+
+const onlyAudioConstraint = {
+  audio: true,
+  video: false,
+};
+
+const defaultConstraint = {
+  audio: true,
+  video: true,
+};
+
+export const getLocalStreamPreview = (onlyAudio = false, callbackFn) => {
+  const constraints = onlyAudio ? onlyAudioConstraint : defaultConstraint;
+
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then((stream) => {
+      // store.dispatch(roomActions.setLocalStream(stream));
+      callbackFn();
+    })
+    .catch((err) => {
+      console.log(err);
+      console.log("Cannot get access to local stream");
+    });
+};
