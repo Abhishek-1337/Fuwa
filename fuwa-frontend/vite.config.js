8984014450355f21ba3cfd49fc3ad1173b150860
Abhishek-1337 <abhishekrinku4@gmail.com>
2024-01-31 12:43:16 +0530

    bug: webRTC secure random generator bug

--- fuwa-frontend/vite.config.js
+++ fuwa-frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import nodePolyfills from "rollup-plugin-polyfill-node";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), nodePolyfills()],
+  define: {
+    global: {},
+  },
+});
