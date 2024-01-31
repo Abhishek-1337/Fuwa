ab8ad5c8e3d16f66e7d0f495967b4d335dbbc549
Abhishek-1337 <abhishekrinku4@gmail.com>
2024-01-31 18:17:03 +0530

    backend: bug-fixed: After reload it didn't disconnect the user from all rooms

--- fuwa-backend/socket handlers/disconnectHandler.js
+++ fuwa-backend/socket handlers/disconnectHandler.js
@@ -2,18 +2,20 @@ const serverStore = require("../serverStore");
 const room = require("./updates/room");
 
 const disconnectHandler = (socket) => {
-  serverStore.removeConnectedUser(socket.id);
-
   const activeRooms = serverStore.getActiveRooms();
   activeRooms.forEach((activeRoom) => {
     const userInRoom = activeRoom.participants.some(
       (participant) => participant.socketId === socket.id
     );
     if (userInRoom) {
-      serverStore.leaveActiveRoom({ roomId: activeRoom.roomId }, socket);
+      serverStore.leaveActiveRoom({
+        roomId: activeRoom.roomId,
+        socketId: socket.id,
+      });
       room.updateRooms();
     }
   });
+  serverStore.removeConnectedUser(socket.id);
 };
 
 module.exports = disconnectHandler;
