a1df092d6acfcb7047d1078cd2248631b0d9aec2
Abhishek-1337 <abhishekrinku4@gmail.com>
2024-01-31 13:09:41 +0530

    frontend: Mic toggle functionality in room

--- fuwa-frontend/src/components/dashboard/Room/RoomButtons/Mic.jsx
+++ fuwa-frontend/src/components/dashboard/Room/RoomButtons/Mic.jsx
@@ -3,9 +3,10 @@ import MicIcon from "@mui/icons-material/Mic";
 import MicOffIcon from "@mui/icons-material/MicOff";
 import { useState } from "react";
 
-const Mic = () => {
-  const [isMicEnabled, setIsMicEnabled] = useState(false);
+const Mic = ({ localStream }) => {
+  const [isMicEnabled, setIsMicEnabled] = useState(true);
   const micToggle = () => {
+    localStream.getAudioTracks()[0].enabled = !isMicEnabled;
     setIsMicEnabled(!isMicEnabled);
   };
   return (
